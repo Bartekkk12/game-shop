ef7c9f5710056be1f7485acedb4b52b99a53385a
Bartekkk12 <pirogbartosz12@gmail.com>
2026-01-12 11:37:59 +0100

    Add database configuration and migration setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using GameShop.Models;
+
+namespace GameShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>()
+                .Property(g => g.GamePlatform)
+                .HasConversion<string>();
+        }
+    }
+}
